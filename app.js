3925156c79e493376614e4ab1bf25914334cd079
MuhammadAsghar1999 <143824430+MuhammadAsghar1999@users.noreply.github.com>
2023-09-11 00:05:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,168 @@
+// var a = parseInt(prompt());
+// var b = parseInt(prompt());
+// console.log(a+b);
+// var userFirstName = prompt("Enter your first name");
+// var userLastName = prompt("Enter your Last name");
+// console.log("hellow" + userFirstName + "" + userLastName);
+// var city = prompt("where do you live");
+// if (city=== "karachi") {
+// console.log("it is one of the beautiful city..")
+// }
+
+
+
+// var percentage = +prompt("Enter your percentage");
+// if (percentage >=80 && percentage <=100) {
+//     alert("A+")
+// }
+
+// if (percentage >=65 && percentage <=79) {
+//     alert("A")
+// }
+
+// if (percentage >=50 && percentage <=64) {
+//     alert("B")
+// }
+
+// if (percentage >=45 && percentage <=49) {
+//     alert("C")
+// }
+
+// if (percentage >=40 && percentage <=44) {
+//     alert("D")
+// }
+// if (percentage >=0 && percentage <=39) {
+//     alert("Failed")
+// }
+
+// var age = +prompt("Enter your age");
+// var weight = +prompt("Enter your weight");
+// var gender = prompt("Enter your Gender")
+//  if (age >=21) 
+//  {
+//    alert("come to try out")
+//  }
+
+ 
+//  if (weight >=21) 
+//  {
+//    alert("come to try out")
+//  }
+
+
+// if ("Gender=male"){
+//     alert("hello brother!")
+// }
+
+
+// class task mathamatic chapter
+// document.write("Result: <br> The value of a is 10 <br>")
+// document.write(".................................. <br><br>")
+// document.write("The value of ++a is:11 <br> Now the value of a is :11 <br><br>")
+// document.write("The value of a++is:11 <br> Now the value of a is :12")
+
+
+
+
+
+// class tast conditional statement
+// Q:1
+// var city = prompt("where do you live");
+// if (city=== "karachi") {
+//  console.log("welcome to city of lights..")
+// }
+// Q:2
+// var gender = prompt("Enter your Gender")
+// if("gender = male"){
+//     alert("Good Morning Sir!!")
+// }
+// var gender = prompt("Enter your Gender")
+// if("gender = Female"){
+//     alert("Good Morning MaM!!")
+// }
+// Q:3
+// var red = prompt("Enter Color");
+//  if (red) {
+//      alert("Must Stop")
+//  }
+//  var yellow = prompt("Enter Color");
+//  if (yellow) {
+//      alert("Ready to Move")
+//  }
+//  var green = prompt("Enter Color");
+//  if (green) {
+//      alert("Move Now")
+//  }
+// Q:4
+// var litter =+prompt("enter fuel average");
+// if (litter===0.25){
+//     alert("Please refill the fuel in ypur car")
+// }
+// Q:5 (A)
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// Q:5 (B)
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// else{
+//     alert("given condition for variable b is false")
+// }
+// Q:5 (C)
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c-- === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(--c === 14){
+// alert("condition 4 is true");
+// }
+// Q:5(D)
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// Q:5(E)
+// if (true){
+//     alert("True");
+//     }
+    // if (false){
+    // alert("False");
+    // }
+    // Q:5(F)
+    // if("car" < "cat"){
+    //     alert("car is smaller than cat");
+    //     }    
+// Q:6
+// var percentage = +prompt("Enter your percentage");
+//  if (percentage >=80 && percentage <=100) {
+//    alert("A-one")}
+
+//    if (percentage >=70 && percentage <=79) {
+//     alert("A") }
+
+//     if (percentage >=60 && percentage <=69) {
+//         alert("B") }
+
+//         if (percentage >= 0 && percentage <=59.99) {
+//             alert("Fail") }
+
+// mark sheet
+
+//  var percentage = +prompt("Enter your percentage");
+//    if (percentage >=73 && percentage <=100) {
+//      document.write("Mark Sheet <br> <br> <br> Total mark:300 <br> Mark Obtain:219 <br> percentage:73% <br> Grade:B <br> Remarks:You Need to improve")}
+
+
+// Q:7
